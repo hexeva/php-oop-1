f6aac1bd3e6268d4c8fee6c8212b747fdd2f7080
hexeva <patrick79.g@gmail.com>
2022-01-27 14:32:54 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP 1</title>
+</head>
+<body>
+<!-- - è definita una classe ‘Movie’
+   => all'interno della classe sono dichiarate delle variabili d'istanza
+   => all'interno della classe è definito un costruttore
+   => all'interno della classe è definito almeno un metodo
+- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà -->
+
+<!-- CREO UNA CLASSE MOVIE -->
+
+<?php
+
+class Movie {
+    public $title
+
+}
+// CREO LE ISTANZE DELLA CLASSE MOVIE
+$terminator = new Movie();
+$rambo = new Movie();
+var_dump($terminator);
+
+?>
+</body>
+</html>
