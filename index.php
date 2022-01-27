ddea5ce4cfc2cdf0800dc14296972ca5f6c381bc
hexeva <patrick79.g@gmail.com>
2022-01-27 16:05:22 +0100

    printed objects

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="css/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OOP 1</title>
 </head>
@@ -25,6 +26,12 @@ class Movie {
     // assegno anche una variabile comune a tutte le istanze
     public $production = 'Metro goldwyn mayer';
 
+    // DEFINISCO UN METODO (che mi restituirà la stringa completa con tutte le caratteristiche dell'istanza)
+
+    public function getMovieComplete() {
+        return $this->title . ' - ' . $this->author . ' - ' . $this->year . ' - ' . $this->production;
+    }
+
 
 }
 // CREO LE ISTANZE DELLA CLASSE MOVIE
@@ -36,7 +43,7 @@ $terminator->year = '1984';
 
 $rambo = new Movie();
 // ad ogni istanza assegno il proprio attributo
-$rambo->title = 'RAMBO first blood';
+$rambo->title = 'RAMBO (first blood)';
 $rambo->author = 'Ted Kotcheff';
 $rambo->year = '1982';
 
@@ -49,8 +56,34 @@ $rocky->year = '1976';
 var_dump($terminator);
 var_dump($rambo);
 var_dump($rocky);
+var_dump($terminator->getMovieComplete());
 
 
 ?>
+
+<div class="container">
+    <div class="movie">
+        <h2 class="info"> <?php echo $terminator->getMovieComplete(); ?> </h2>
+        <h2> <?php echo $terminator->title; ?> </h2>
+        <h3> <?php echo $terminator->author; ?> </h3>
+        <h4> <?php echo $terminator->year; ?> </h4>
+        <h5> <?php echo $terminator->production ?> </h5>
+    </div>
+    <div class="movie">
+    <h2 class="info"> <?php echo $rambo->getMovieComplete(); ?> </h2>
+        <h2> <?php echo $rambo->title; ?> </h2>
+        <h3> <?php echo $rambo->author; ?> </h3>
+        <h4> <?php echo $rambo->year; ?> </h4>
+        <h5> <?php echo $rambo->production; ?> </h5>
+    </div>
+    <div class="movie">
+    <h2 class="info"> <?php echo $rocky->getMovieComplete(); ?></h2>
+        <h2> <?php echo $rocky->title ?> </h2>
+        <h3> <?php echo $rocky->author ?> </h3>
+        <h4> <?php echo $rocky->year ?> </h4>
+        <h5> <?php echo $rocky->production ?> </h5>
+    </div>
+</div>
+
 </body>
 </html>
